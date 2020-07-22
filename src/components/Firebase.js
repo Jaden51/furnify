ac1d65be59c7b4911e2c4c91908e982ddb91e459
Jaden51 <jadenhums51@gmail.com>
2020-07-22 01:10:05 -0400

    Added backend using firebase database

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -15,17 +15,73 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Jaden Hums',
-    age: 18,
-    isSingle: false,
-    location: {
-        city: 'Newmarket',
-        country: 'Canada'
-    }
-});
+const storage = firebase.storage();
 
-database.ref('attributes').set({
-    height: 70,
-    weight: 140
-});
+export { storage, database, firebase as default }
+
+// database.ref('furniture').push({
+//     image: about,
+//     description: 'chair'
+// })
+
+// database.ref('furniture').on('value', (snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+
+//     console.log(expenses);
+// });
+
+// database.ref().on('value', (snapshot) => {
+//     const val = snapshot.val();
+//     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
+// })
+
+// database.ref('location')
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log('Error fetching data', e);
+//     });
+
+// database.ref().set({
+//     name: 'Jaden Hums',
+//     age: 18,
+//     stressLevel: 3,
+//     job: {
+//         title: 'Software Engineer',
+//         company: 'Google'
+//     },
+//     isSingle: false,
+//     location: {
+//         city: 'Newmarket',
+//         country: 'Canada'
+//     }
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((e) => {
+//     console.log('This failed.', e);
+// })
+
+// database.ref().update({
+//     'location/city': 'Seattle',
+//     'location/country': 'United States',
+//     'job/company': 'Amazon',
+//     stressLevel: 9
+// });
+
+// database.ref()
+//     .remove()
+//     .then(() => {
+//         console.log('Data was removed');
+//     }).catch((e) => {
+//         console.log('Data was not removed', e);
+//     })
