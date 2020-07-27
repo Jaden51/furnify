708ad59bb54f1959811bf7fe8e8f0d548cd01185
Jaden51 <jadenhums51@gmail.com>
2020-07-26 23:07:37 -0400

    Added Redux Store to app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
-// import * as actions from './store/actions/index';
 
 const store = configureStore();
 
@@ -12,9 +11,3 @@ ReactDOM.render(
         <App />
     </Provider>,
     document.getElementById('root'));
-
-// store.subscribe(() => {
-//     console.log(store.getState());
-// });
-
-// store.dispatch(actions.addProduct({ id: 'temp link', weight: 110, category: 'chair' }));
