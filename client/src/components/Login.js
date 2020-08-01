dafa97469d9f599c8383d17de0956770d505deff
Jaden51 <jadenhums51@gmail.com>
2020-07-31 23:14:03 -0400

    Added login and sign up forms

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,66 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { startLogin } from '../store/actions/auth';
+import React, { Component } from 'react';
+import { Form, Formik, Field } from 'formik';
+import { Link } from 'react-router-dom';
 
-export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
-    </div>
-);
+class Login extends Component {
+    constructor(props) {
+        super(props);
 
-const mapDispatchToProps = dispatch => ({
-    startLogin: () => dispatch(startLogin())
-});
+        this.state = {
+            formData: {
+                email: '',
+                password: '',
+            }
+        }
+    }
 
-export default connect(undefined, mapDispatchToProps)(LoginPage);
+    handleChange = ({ target }) => {
+        const { formData } = this.state;
+        formData[target.name] = target.value;
+        this.setState({ formData });
+    }
 
+    handleSubmit = async () => {
+        console.log('Success')
+    }
+
+    render() {
+        return (
+            <Formik 
+                initialValues={this.state}
+                onSubmit={this.handleSubmit}
+            >
+                <Form>
+                    <h2>Sign In</h2>
+                    <h3>Sign Into Your Account</h3>
+                    <Field
+                        type='text'
+                        placeholder='Email Address'
+                        name='email'
+                        className='input name-input'
+                        required
+                        onChange={this.handleChange}
+                    /><br />
+                    <Field
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        className='input pass-input'
+                        required
+                        onChange={this.handleChange}
+                    /><br />
+                    <input 
+                        type='submit'
+                        className='btn' 
+                        value='Login'
+                    />
+                    <p>
+                        Don't have an account? <Link to='/signup'>Sign Up</Link>
+                    </p>
+                </Form>
+            </Formik>
+        )
+    }
+}
+
+export default Login;
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,11 +1,87 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import { Form, Formik, Field } from 'formik';
+import { Link } from 'react-router-dom';
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            formData: {
+                name: '',
+                email: '',
+                password: '',
+                confirmPass: ''
+            }
+        }
+    }
+
+    handleChange = ({ target }) => {
+        const { formData } = this.state;
+        formData[target.name] = target.value;
+        this.setState({ formData });
+    }
+
+    handleSubmit = async () => {
+        const { password, confirmPass } = this.state.formData;
+        if (password !== confirmPass) {
+            console.log('Passwords do not match');
+        } else {
+            console.log(this.state.formData)
+        }
+    }
+
     render() {
-        return(
-            <div>
-                <h1>SignUp Page</h1>
-            </div>
+        return (
+            <Formik 
+                initialValues={this.state}
+                onSubmit={this.handleSubmit}
+            >
+                <Form>
+                    <h2>Sign Up</h2>
+                    <h3>Create Your Account</h3>
+                    <Field
+                        type='text'
+                        placeholder='Name'
+                        name='name'
+                        className='input name-input'
+                        required
+                        onChange={this.handleChange}
+                    /><br />
+                    <Field
+                        type='text'
+                        placeholder='Email Address'
+                        name='email'
+                        className='input name-input'
+                        required
+                        onChange={this.handleChange}
+                    /><br />
+                    <Field
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        className='input pass-input'
+                        required
+                        onChange={this.handleChange}
+                    /><br />
+                    <Field
+                        type='password'
+                        placeholder='Confirm Password'
+                        name='confirmPass'
+                        className='input confirm-input'
+                        required
+                        onChange={this.handleChange}
+                    /><br />
+                    <input 
+                        type="submit" 
+                        className="btn btn-primary" 
+                        value="Register"
+                    />
+                    <p className="my-1">
+                        Already have an account? <Link to="/login">Sign In</Link>
+                    </p>
+                </Form>
+            </Formik>
         )
     }
 }
